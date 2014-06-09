4532f9d4429aa699dd25b77f0f64d7c03bceeec7
milyges <milyges@gmail.com>
2014-06-09 16:51:39 +0200

    Added MySQL Support

--- uberbackup.sh
+++ uberbackup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Glowny katalog backupu
-BACKUPDIR="/mnt/backup/filesystem"
+BACKUPDIR="/mnt/backup/uberbackup"
 
 # Podkatalog na aktualny backup
 CURRENTDIR="$(date '+%d-%m-%Y')"
@@ -45,11 +45,47 @@ rotate() {
 	touch "${1}/${3}" 2>&1 | log
 }
 
+# katalog_docelowy katalog_zrodlowy
 backup_fs() {
-	echo "Starting backup of ${2} to ${1}" | log
+	echo "Backing up ${2} to ${1}" | log
 	mkdir -p "${1}${2}" 2>&1 | log
 	${IONICE} rsync -aAX --delete "${2}" "${1}${2}" 2>&1 | log
 	echo "Backup ${2} finished" | log
+}
+
+# katalog_docelowy login haslo [baza]
+backup_mysql() {
+	DBS=""
+	if [ -z "${4}" ]
+	then
+		DBS="$(echo SHOW DATABASES | mysql --user="${2}" --password="${3}" -s)"
+	else
+		DBS="${4}"
+	fi
+
+	for DB in ${DBS}
+	do
+		echo "Backing up MySQL database ${DB}" | log
+
+		if [ "$DB" = "mysql" ] || [ $DB = "information_schema" ] || [ "${DB}" = "performance_schema" ]
+		then
+			continue
+		fi
+
+		rm -f "${1}/mysql/${DB}.sql.bz2" 2>&1 | log
+		rm -f "${1}/mysql/${DB}.sql" 2>&1 | log
+
+		mkdir -p "${1}/mysql" 2>&1 | log
+		TMPLOG="$(mktemp)"
+		mysqldump --user="${2}" --password="${3}" "${DB}" > "${1}/mysql/${DB}.sql" 2> "${TMPLOG}"
+		[ $? -ne 0 ] && rm "${1}/mysql/${DB}.sql" || bzip2 "${1}/mysql/${DB}.sql" 2>&1 | log
+		cat "${TMPLOG}" | log
+		rm -f "${TMPLOG}"
+		if [ ! -s "${1}/mysql/${DB}.sql.bz2" ]
+		then
+			echo "ERROR: ${DB}.sql not bzipped!" | log
+		fi
+	done
 }
 
 echo "Starting backup on $(hostname)" | log
@@ -64,9 +100,11 @@ fi
 
 rotate "${BACKUPDIR}" "${KEEP}" "${CURRENTDIR}"
 
-backup_fs "${BACKUPDIR}/${CURRENTDIR}" "/etc/"
-backup_fs "${BACKUPDIR}/${CURRENTDIR}" "/usr/local/"
-backup_fs "${BACKUPDIR}/${CURRENTDIR}" "/home/"
+#backup_fs "${BACKUPDIR}/${CURRENTDIR}" "/etc/"
+#backup_fs "${BACKUPDIR}/${CURRENTDIR}" "/usr/local/"
+#backup_fs "${BACKUPDIR}/${CURRENTDIR}" "/home/"
+
+#backup_mysql "${BACKUPDIR}/${CURRENTDIR}" "backup" "***"
 
 mail -s "UberBackup: finished on $(hostname)" "${MAILTO}" < "${LOGFILE}"
 rm "${LOGFILE}"
